4488192731801fd7e830848b4fa136eda857b677
sonia <sonia@MacBook-Pro-de-sonia-2.local>
2019-10-10 06:04:42 +0200

    css

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,20 @@
 'use strict';
-
+/* 
 const toggleBurgerMenu = (event) => {
   //console.log(event.target.parentElement);
   const menu = document.getElementById("nav-menu");
   const burgerMenuIcon = event.target.parentElement;
   burgerMenuIcon.classList.toggle("burger-menu-icon-on");
   menu.classList.toggle("hide-menu");
-}
+} */
+
+
+
+function classToggle() {
+  const navs = document.querySelectorAll('.Navbar_Items')
+  
+  navs.forEach(nav => nav.classList.toggle('Navbar_ToggleShow'));
+  }
+  
+  document.querySelector('.Navbar-small')
+  .addEventListener('click', classToggle);
