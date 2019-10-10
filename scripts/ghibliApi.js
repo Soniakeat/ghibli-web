e33ee8f10c03002c17b588b4c9745f481fbd1e4e
sonia <sonia@MacBook-Pro-de-sonia-2.local>
2019-10-10 16:18:37 +0200

    card films

--- scripts/ghibliApi.js
+++ scripts/ghibliApi.js
@@ -10,12 +10,12 @@ async function getUsers() {
         card.innerHTML = `<img src="images/images/le-tombeau-des-lucioles.jpeg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title font-weight-bold"> ${data[i].title}</h5>
-            <p class="card-text">${data[i].description}</p>
+            <p class="card-text font-weight-light">${data[i].description}</p>
         </div>
         <ul class="list-group list-group-flush">
-    <li class="list-group-item"><small class="text-muted">RELEASE DATE : ${data[i].release_date}</small></li>
-    <li class="list-group-item"><small class="text-muted">DIRECTOR : ${data[i].director}</small></li>
-    <li class="list-group-item"><small class="text-muted">RATE SCORE : ${data[i].rt_score}</small></li>
+    <li class="list-group-item"><small class="text-muted font-weight-bold">RELEASE DATE : ${data[i].release_date}</small></li>
+    <li class="list-group-item"><small class="text-muted font-weight-bold">DIRECTOR : ${data[i].director}</small></li>
+    <li class="list-group-item"><small class="text-muted font-weight-bold">RATE SCORE : ${data[i].rt_score}</small></li>
   </ul>`
         div.appendChild(card)
 
