5ffa9593ca174a459af0be1f03a3166b5dc41979
sonia <sonia@MacBook-Pro-de-sonia-2.local>
2019-10-10 12:42:33 +0200

    responsive

--- scripts/ghibliApi.js
+++ scripts/ghibliApi.js
@@ -9,15 +9,19 @@ async function getUsers() {
         card.setAttribute("class", "card")
         card.innerHTML = `<img src="images/images/le-tombeau-des-lucioles.jpeg" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">${data[i].title}</h5>
+            <h5 class="card-title"> ${data[i].title}</h5>
             <p class="card-text">${data[i].description}</p>
-            <p class="card-text"><small class="text-muted">${data[i].release_date}</small></p>
-            <p class="card-text"><small class="text-muted">${data[i].director}</small></p>
-        </div>`
+        </div>
+        <ul class="list-group list-group-flush">
+    <li class="list-group-item"><small class="text-muted">RELEASE DATE : ${data[i].release_date}</small></li>
+    <li class="list-group-item"><small class="text-muted">DIRECTOR : ${data[i].director}</small></li>
+    <li class="list-group-item"><small class="text-muted">RATE SCORE : ${data[i].rt_score}</small></li>
+  </ul>`
         div.appendChild(card)
-        console.log(card);
+
     }
    
     console.log(data);
 }
 getUsers();
+
