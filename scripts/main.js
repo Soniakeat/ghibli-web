9c31ad2de63d83b3234313b859b274d5e39c45d8
Soniakeat <soniakeating@hotmail.fr>
2020-02-20 23:47:35 +0100

    refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,4 @@
 'use strict';
-/* 
-const toggleBurgerMenu = (event) => {
-  //console.log(event.target.parentElement);
-  const menu = document.getElementById("nav-menu");
-  const burgerMenuIcon = event.target.parentElement;
-  burgerMenuIcon.classList.toggle("burger-menu-icon-on");
-  menu.classList.toggle("hide-menu");
-} */
-
-
 
 function classToggle() {
   const navs = document.querySelectorAll('.Navbar_Items')
